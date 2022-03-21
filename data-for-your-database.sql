c2adce89ca1c5c2435034ea38a342ab129a478fa
Fayaz <d00217205@student.dkit.ie>
2022-03-21 14:48:05 +0000

    Added Colour table

--- data-for-your-database.sql
+++ data-for-your-database.sql
@@ -1,14 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Feb 20, 2022 at 06:31 PM
--- Server version: 5.7.34
--- PHP Version: 7.4.21
-DROP DATABASE categories;
-CREATE DATABASE IF NOT EXISTS `categories` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `categories`;
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2022 at 03:45 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -21,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ca2`
+-- Database: `categories`
 --
 
 -- --------------------------------------------------------
@@ -54,6 +51,7 @@ CREATE TABLE `records` (
   `recordID` int(11) NOT NULL,
   `categoryID` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `Colour` varchar(20) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -62,19 +60,19 @@ CREATE TABLE `records` (
 -- Dumping data for table `records`
 --
 
-INSERT INTO `records` (`recordID`, `categoryID`, `name`, `price`, `image`) VALUES
-(1, 1, 'Iphone 6', '120', '001.png'),
-(2, 1, 'Iphone 6s', '150', '001.png'),
-(3, 1, 'Iphone 6s Plus', '180', '001.png'),
-(4, 1, 'Iphone 7', '200', '001.png'),
-(5, 2, 'Samsung s6', '220', '001.png'),
-(6, 2, 'Samsung s7', '250', '001.png'),
-(7, 2, 'Samsung s8', '280', '001.png'),
-(8, 2, 'Samsung s9', '300', '001.png'),
-(9, 2, 'Samsung s10', '350', '001.png'),
-(10, 3, 'Huawei P60', '380', '001.png'),
-(11, 3, 'Huawei P70', '360', '001.png'),
-(12, 3, 'Huawei P80', '400', '001.png');
+INSERT INTO `records` (`recordID`, `categoryID`, `name`, `Colour`, `price`, `image`) VALUES
+(1, 1, 'Iphone 6', 'Black', '130.00', '001.png'),
+(2, 1, 'Iphone 6s', 'Black', '150.00', '001.png'),
+(3, 1, 'Iphone 6s Plus', 'Black', '180.00', '001.png'),
+(4, 1, 'Iphone 7', 'White', '200.00', '001.png'),
+(5, 2, 'Samsung s6', 'Blue', '220.00', '001.png'),
+(6, 2, 'Samsung s7', 'Black', '250.00', '001.png'),
+(7, 2, 'Samsung s8', 'White', '280.00', '001.png'),
+(8, 2, 'Samsung s9', 'Gold', '300.00', '001.png'),
+(9, 2, 'Samsung s10', 'White', '350.00', '001.png'),
+(10, 3, 'Huawei P60', 'Black', '380.00', '001.png'),
+(11, 3, 'Huawei P70', 'Gold', '360.00', '001.png'),
+(12, 3, 'Huawei P80', 'Black', '400.00', '001.png');
 
 --
 -- Indexes for dumped tables
@@ -100,7 +98,7 @@ ALTER TABLE `records`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `records`
@@ -112,5 +110,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
