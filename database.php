45775cbbf06f77fa8252f88c5ec855c79b804ccd
Fayaz <d00217205@student.dkit.ie>
2022-03-21 16:04:43 +0000

    Changed Database name and Password

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=categories';  //the db name is D00217205 because on host, the database name is that, otherwise use whatever the dbname is.
-    $username = 'root'; // this is only used when it is connected to hosting
-    $password = '';  // this is only used when it is connected to hosting
+    $dsn = 'mysql:host=localhost;dbname=D00217205';  //the db name is D00217205 because on host, the database name is that, otherwise use whatever the dbname is.
+    $username = 'D00217205'; // this is only used when it is connected to hosting
+    $password = 'K1rgEu8c';  // this is only used when it is connected to hosting
 
     try {
         $db = new PDO($dsn, $username, $password);
